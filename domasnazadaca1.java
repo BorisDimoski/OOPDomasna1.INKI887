3ce540936270bac63861277f341e4a0e8967abfc
BorisDimoski <96881472+BorisDimoski@users.noreply.github.com>
2022-10-31 13:35:03 +0100

    change

--- domasnazadaca1.java
+++ domasnazadaca1.java
@@ -3,7 +3,7 @@ package domasnazadaca1;
 public class domasnazadaca1 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");	
+		System.out.println("Hello World OOP FIKT");	
 
 	}
 
